94d5695dd9224af6f7c5c4cb68f9f1cdbc05b230
joonaspuuppo <joonas.puuppo@gmail.com>
2025-04-06 21:36:44 +0300

    chore: initial setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { BrowserRouter } from 'react-router'
+import { Provider } from '@/components/ui/provider'
 import App from './App.tsx'
+import './assets/fonts/Reactor7.woff'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+      <Provider>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </StrictMode>
 )
