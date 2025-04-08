5127f761f3b8f12ea0edfb85ef392703ee98d1b6
joonaspuuppo <joonas.puuppo@gmail.com>
2025-04-09 00:21:44 +0300

    feat: add dynamic max height to text area, responsive layout, text to cloud

--- src/pages/SpeakLikeCloud.tsx
+++ src/pages/SpeakLikeCloud.tsx
@@ -1,36 +1,45 @@
-import { addPixels } from '@/util/common'
 import { Box, Center, Heading, Image, Textarea, VStack } from '@chakra-ui/react'
+import { useEffect, useRef, useState } from 'react'
 
-const BACKGROUND_WIDTH = '800px'
-const BACKGROUND_HEIGHT = '600px'
+const MAX_WIDTH = '1000px'
+
+const cloudTexts = [
+  "Barret\n\u201CThis mission was a success. But don't get lazy now. The hard part's still to come! Don't y'all be scared of that explosion! Cause the next one's gonna be bigger than that! Meet back at the hideout!! Move out!\u201D",
+]
 
 const SpeakLikeCloud = () => {
+  const bgImageRef = useRef<HTMLImageElement>(null)
+  const [bgImageHeight, setBgImageHeight] = useState<number | undefined>(0)
+  const [cloudText, setCloudText] = useState<string>(cloudTexts[0])
+
+  useEffect(() => {
+    setBgImageHeight(bgImageRef.current?.clientHeight)
+  }, [setBgImageHeight])
+
   return (
     <Center>
-      <VStack spaceY={5}>
+      <VStack spaceY={5} w={'full'} maxW={MAX_WIDTH}>
         <Heading as={'h1'} fontSize={'28px'}>
           Speak like Cloud
         </Heading>
         <Box position={'relative'}>
           <Image
             src="/ffvii_background.jpg"
-            w={BACKGROUND_WIDTH}
-            h={BACKGROUND_HEIGHT}
+            w={'full'}
+            aspectRatio={{ base: 3 / 4, md: 4 / 3 }}
+            ref={bgImageRef}
           />
-          <Box
-            position={'absolute'}
-            top={0}
-            left={0}
-            w={BACKGROUND_WIDTH}
-            h={BACKGROUND_HEIGHT}
-          >
-            <Center mt={'20px'}>
+          <Box position={'absolute'} top={0} left={0} w={'full'} h={'full'}>
+            <Center mt={'40px'}>
               <Textarea
+                mx={10}
                 autoresize
                 variant={'cloud'}
-                w={'600px'}
+                w={'full'}
                 minH={'100px'}
-                maxH={addPixels(BACKGROUND_HEIGHT, -40)}
+                maxH={bgImageHeight && bgImageHeight - 80}
+                value={cloudText}
+                onChange={(e) => setCloudText(e.target.value)}
               />
             </Center>
           </Box>
